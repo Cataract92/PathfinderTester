92488a6eb655d23334c142c838fad4968f1cef4a
Cataract <salatgurke92@hotmail.de>
2018-03-02 17:23:41 +0100

    added MinPlus

--- src/PathfinderTester.java
+++ src/PathfinderTester.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -122,12 +123,15 @@ public class PathfinderTester {
             openList.remove(currentField);
 
             if (currentField == currentLevel.endPoint){
+                /*
                 Field prev = precessors.get(currentLevel.endPoint);
                 while (prev != currentLevel.startPoint)
                 {
                     jPanels[prev.x][prev.y].setBackground(Color.GREEN);
                     prev = precessors.get(prev);
                 }
+
+                */
                 break;
             }
 
@@ -172,9 +176,11 @@ public class PathfinderTester {
         }
     }
 
+
     public static void testMinPlus()
     {
-        HashMap<Field,HashMap<Field,Double>> costMatrix = new HashMap<>();
+
+        double[][] costMatrix = new double[20*20][20*20];
 
         for (int i = 0; i<currentLevel.map.length; i++)
         {
@@ -182,7 +188,6 @@ public class PathfinderTester {
             {
 
                 Field a = currentLevel.map[i][j];
-                costMatrix.put(a,new HashMap<>());
 
                 for (int k = 0; k<currentLevel.map.length; k++)
                 {
@@ -191,43 +196,88 @@ public class PathfinderTester {
                         Field b = currentLevel.map[k][l];
 
                         if (a == b) {
-                            costMatrix.get(a).put(b, 0d);
+                            costMatrix[a.x+20*a.y][b.x+20*b.y] = 0;
                             continue;
                         }
 
                         if (b.status == -1) {
-                            costMatrix.get(a).put(b, Double.MAX_VALUE);
+                            costMatrix[a.x+20*a.y][b.x+20*b.y] = Double.MAX_VALUE;
                             continue;
                         }
 
                         if (Math.sqrt(Math.pow(a.x-b.x,2) + Math.pow(a.y-b.y,2)) <= Math.sqrt(2)) {
-                            costMatrix.get(a).put(b, 1d);
+                            costMatrix[a.x+20*a.y][b.x+20*b.y] =  Math.sqrt(Math.pow(a.x-b.x,2) + Math.pow(a.y-b.y,2));
                             continue;
                         }
 
-                        costMatrix.get(a).put(b,Double.MAX_VALUE);
+                        costMatrix[a.x+20*a.y][b.x+20*b.y] = Double.MAX_VALUE;
                     }
                 }
             }
         }
 
-        double[][] endMatrix = new double[20][20];
+        double[][] prev = costMatrix;
 
-        for (Field a : costMatrix.keySet())
-        {
-            for (Field b : costMatrix.get(a).keySet())
+
+        do{
+
+            double[][] tmp = new double[20*20][20*20];
+            for (int i = 0; i < 20*20; i++)
             {
-                for (int l = 0; l < 20; l++)
+                for (int j = 0; j < 20*20; j++)
                 {
+                    double min = Double.MAX_VALUE;
+                    for (int l = 0; l < 20*20; l++)
+                    {
+                        if (prev[i][l] + costMatrix[l][j] < min)
+                        {
+                            min = prev[i][l] + costMatrix[l][j];
+                        }
+                    }
+                    tmp[i][j] = min;
                 }
             }
+
+            prev = costMatrix;
+            costMatrix = tmp;
+
+        } while (!Arrays.deepEquals(prev,costMatrix));
+
+        System.out.println("No Loop!");
+
+        Field tmp = currentLevel.endPoint;
+        while (tmp != currentLevel.startPoint)
+        {
+            Field min = tmp;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    try {
+                        if (costMatrix[currentLevel.startPoint.x + 20*currentLevel.startPoint.y][(tmp.x+i)+20*(tmp.y+j)] < costMatrix[currentLevel.startPoint.x + 20*currentLevel.startPoint.y][(min.x)+20*(min.y)])
+                            min = currentLevel.map[tmp.x+i][tmp.y+j];
+                    }catch (ArrayIndexOutOfBoundsException e)
+                    {
+                        continue;
+                    }
+                }
+            }
+            tmp = min;
+
+            if (tmp != currentLevel.startPoint)
+                jPanels[min.x][min.y].setBackground(Color.GREEN);
+
         }
 
-        for (int i = 0; i<currentLevel.map.length; i++) {
-            for (int j = 0; j < currentLevel.map[0].length; j++) {
-                System.out.print(costMatrix.get(currentLevel.map[0][4]).get(currentLevel.map[i][j]).intValue()+ " ");
-            }
-            System.out.println("");
+    }
+
+    public static class Matrix
+    {
+        public double[][] m;
+
+        public Matrix(int width, int height)
+        {
+            m = new double[width*height][width*height];
         }
     }
 
